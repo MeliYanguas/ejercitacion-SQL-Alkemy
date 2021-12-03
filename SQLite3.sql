875e7255e268981267556335d674ffb2727614fe
MeliYanguas <63035921+MeliYanguas@users.noreply.github.com>
2021-12-03 13:49:13 -0300

    ejercitacion queries

---
+++ SQLite3.sql
@@ -0,0 +1,20 @@
+-- 1)
+SELECT nombre, apellido, fecha_nacimiento from profesor
+ORDER by fecha_nacimiento
+-- 2)
+SELECT * from profesor WHERE salario >= 65000
+
+-- 3)
+SELECT * from profesor WHERE fecha_nacimiento BETWEEN 1980 and 1990
+
+-- 4)
+SELECT * FROM profesor LIMIT 5;
+
+-- 5) 
+SELECT * FROM profesor
+WHERE apellido LIKE '%P%'
+
+-- 6)
+SELECT * from profesor 
+WHERE fecha_nacimiento BETWEEN 1980 and 1990 
+and salario > 80000
