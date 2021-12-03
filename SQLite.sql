2959273887f8d81bb4493eb951d3ea641889ce2d
MeliYanguas <63035921+MeliYanguas@users.noreply.github.com>
2021-12-02 21:54:55 -0300

    ejercicio 1 tablas

---
+++ SQLite.sql
@@ -0,0 +1,27 @@
+CREATE TABLE curso (
+codigo INT NOT NULL,  
+nombre VARCHAR(100) NOT NULL,
+descripcion VARCHAR(200),
+turno VARCHAR(100) NOT NULL,
+PRIMARY KEY (codigo)
+);
+
+ALTER TABLE curso ADD cupo INt;
+
+INSERT INTO curso (codigo, nombre, descripcion, turno, cupo) 
+VALUES (101, "Algoritmos","Algoritmos y Estructuras de Datos","Mañana",35);
+  
+INSERT INTO curso (codigo, nombre, descripcion, turno, cupo) 
+VALUES (102, "Matemática Discreta","","Tarde",30);
+
+INSERT INTO curso (codigo, nombre, descripcion, turno, cupo) 
+VALUES (50, NULL,"","Tarde",30);                                     
+-- NOT NULL constraint failed: curso.nombre
+
+INSERT INTO curso (codigo, nombre, descripcion, turno, cupo) 
+VALUES (102, "Matemática Discreta","","Tarde",30);
+-- UNIQUE constraint failed: curso.codigo
+
+UPDATE curso SET cupo = 25;
+
+DELETE FROM curso WHERE codigo = 101;
